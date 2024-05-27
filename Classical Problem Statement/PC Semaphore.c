c593a4362278561a264b9d4f98743ae69bf63325
Pranav Bhawari <142681486+Pranav6197@users.noreply.github.com>
2024-05-27 09:11:04 +0530

    Update PC Semaphore.c

--- Classical Problem Statement/PC Semaphore.c
+++ Classical Problem Statement/PC Semaphore.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h> // for sleep()
 
-#define bs 8
+#define BUFFER_SIZE 8
+#define MAX_ITEMS 20 // Maximum number of items to produce for demonstration
 
-sem_t es;
-sem_t fs;
-int buffer[bs];
+sem_t empty_slots;
+sem_t full_slots;
+int buffer[BUFFER_SIZE];
 int in = 0, out = 0;
+int item_count = 0; // Count of produced items for termination
 
 void *producer(void *arg)
 {
     int item = 1;
 
-    while (1)
+    while (item_count < MAX_ITEMS)
     {
-
         printf("Produced item: %d\n", item);
 
-        sem_wait(&es);
+        sem_wait(&empty_slots);
 
         buffer[in] = item;
-        in = (in + 1) % bs;
+        in = (in + 1) % BUFFER_SIZE;
 
-        sem_post(&fs);
+        sem_post(&full_slots);
 
         item++;
+        item_count++;
+        sleep(1); // Add sleep to slow down production for demo
     }
 
     pthread_exit(NULL);
@@ -33,17 +37,21 @@ void *producer(void *arg)
 
 void *consumer(void *arg)
 {
-    while (1)
-    {
+    int consumed_count = 0;
 
-        sem_wait(&fs);
+    while (consumed_count < MAX_ITEMS)
+    {
+        sem_wait(&full_slots);
 
         int consumed = buffer[out];
         printf("Consumed item: %d\n", consumed);
 
-        out = (out + 1) % bs;
+        out = (out + 1) % BUFFER_SIZE;
 
-        sem_post(&es);
+        sem_post(&empty_slots);
+
+        consumed_count++;
+        sleep(1); // Add sleep to slow down consumption for demo
     }
 
     pthread_exit(NULL);
@@ -51,18 +59,18 @@ void *consumer(void *arg)
 
 int main()
 {
-    pthread_t producerThread, consumerThread;
-    sem_init(&es, 0, bs);
-    sem_init(&fs, 0, 0);
+    pthread_t producer_thread, consumer_thread;
+    sem_init(&empty_slots, 0, BUFFER_SIZE);
+    sem_init(&full_slots, 0, 0);
 
-    pthread_create(&producerThread, NULL, producer, NULL);
-    pthread_create(&consumerThread, NULL, consumer, NULL);
+    pthread_create(&producer_thread, NULL, producer, NULL);
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
 
-    pthread_join(producerThread, NULL);
-    pthread_join(consumerThread, NULL);
+    pthread_join(producer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
 
-    sem_destroy(&es);
-    sem_destroy(&fs);
+    sem_destroy(&empty_slots);
+    sem_destroy(&full_slots);
 
     return 0;
 }
